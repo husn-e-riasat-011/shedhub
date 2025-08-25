7f0dc731cc79e401144ab0164466cd83fd36fe98
Muhammad Adeel Shahid <adeelshahid311@gmail.com>
2025-08-25 08:28:41 -0700

    HOOKS

--- src/pages/allsheds.jsx
+++ src/pages/allsheds.jsx
@@ -6,6 +6,10 @@ import { FaList, FaTh } from "react-icons/fa";
 import Footer from "../component/footer";
 import PropertyListings from "../component/links";
 
+import { useSheds } from "../hooks/useSheds";
+
+
+
 const shedsData = [
   {
     id: 1,
@@ -59,6 +63,7 @@ const shedsData = [
 
 const Allsheds = () => {
   const [sheds] = useState(shedsData);
+  const { allsheds, loading, error } = useSheds();
 
   // ✅ Filter States
   const [category, setCategory] = useState("");
